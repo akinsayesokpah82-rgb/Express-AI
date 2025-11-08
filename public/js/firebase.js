bc20470e646dfe29ff1e4a7c5e6f839e5a5fd30d
akinsayesokpah82-rgb <akinsayesokpah82@gmail.com>
2025-11-08 01:26:04 +0000

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -4,10 +4,10 @@ import {
   getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import {
-  getFirestore, collection, addDoc, query, orderBy, onSnapshot,
-  serverTimestamp, doc, setDoc, updateDoc, getDoc, getDocs
+  getFirestore, collection, addDoc, doc, setDoc, query, orderBy, onSnapshot, serverTimestamp, getDocs
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// Your Firebase config (you said it's configured)
 const firebaseConfig = {
   apiKey: "AIzaSyC7cAN-mrE2PvmlQ11zLKAdHBhN7nUFjHw",
   authDomain: "fir-u-c-students-web.firebaseapp.com",
@@ -16,19 +16,19 @@ const firebaseConfig = {
   messagingSenderId: "113569186739",
   appId: "1:113569186739:web:d8daf21059f43a79e841c6"
 };
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
 
-// helper to mark presence/typing in `presence/{uid}`
-export async function setPresence(uid, data) {
-  const ref = doc(db, "presence", uid);
-  await setDoc(ref, { ...data, lastSeen: serverTimestamp() }, { merge: true });
+// Auth helpers
+export async function googleLogin() {
+  await signInWithPopup(auth, provider);
 }
-export async function clearPresence(uid) {
-  const ref = doc(db, "presence", uid);
-  await setDoc(ref, { online: false, typing: false, lastSeen: serverTimestamp() }, { merge: true });
+export async function googleLogout() {
+  await signOut(auth);
 }
-export async function googleLogin() { await signInWithPopup(auth, provider); }
-export async function googleLogout() { await signOut(auth); }
+export function onAuth(cb) {
+  onAuthStateChanged(auth, cb);
+}

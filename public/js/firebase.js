9755737e76235e76900e5f85f2c5bd19200dc614
akinsayesokpah82-rgb <akinsayesokpah82@gmail.com>
2025-11-08 04:04:45 +0000

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -8,12 +8,12 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC7cAN-mrE2PvmlQ11zLKAdHBhN7nUFjHw",
-  authDomain: "fir-u-c-students-web.firebaseapp.com",
-  projectId: "fir-u-c-students-web",
-  storageBucket: "fir-u-c-students-web.firebasestorage.app",
-  messagingSenderId: "113569186739",
-  appId: "1:113569186739:web:d8daf21059f43a79e841c6"
+  apiKey: (typeof FIREBASE_API_KEY !== 'undefined') ? FIREBASE_API_KEY : "AIzaSyC7cAN-mrE2PvmlQ11zLKAdHBhN7nUFjHw",
+  authDomain: (typeof FIREBASE_AUTH_DOMAIN !== 'undefined') ? FIREBASE_AUTH_DOMAIN : "fir-u-c-students-web.firebaseapp.com",
+  projectId: (typeof FIREBASE_PROJECT_ID !== 'undefined') ? FIREBASE_PROJECT_ID : "fir-u-c-students-web",
+  storageBucket: (typeof FIREBASE_STORAGE_BUCKET !== 'undefined') ? FIREBASE_STORAGE_BUCKET : "fir-u-c-students-web.firebasestorage.app",
+  messagingSenderId: (typeof FIREBASE_MESSAGING_SENDER_ID !== 'undefined') ? FIREBASE_MESSAGING_SENDER_ID : "113569186739",
+  appId: (typeof FIREBASE_APP_ID !== 'undefined') ? FIREBASE_APP_ID : "1:113569186739:web:d8daf21059f43a79e841c6"
 };
 
 const app = initializeApp(firebaseConfig);

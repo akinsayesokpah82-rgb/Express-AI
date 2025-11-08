c45889c69d6e847254042c71627c65a164d45cab
akinsayesokpah82-rgb <akinsayesokpah82@gmail.com>
2025-11-08 00:33:37 +0000

    Create firebase.js

---
+++ public/js/firebase.js
@@ -0,0 +1,25 @@
+// public/js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } 
+  from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7cAN-mrE2PvmlQ11zLKAdHBhN7nUFjHw",
+  authDomain: "fir-u-c-students-web.firebaseapp.com",
+  projectId: "fir-u-c-students-web",
+  storageBucket: "fir-u-c-students-web.firebasestorage.app",
+  messagingSenderId: "113569186739",
+  appId: "1:113569186739:web:d8daf21059f43a79e841c6"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+
+export async function googleLogin() {
+  await signInWithPopup(auth, provider);
+}
+
+export async function googleLogout() {
+  await signOut(auth);
+}

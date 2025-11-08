043fca6197be87e365f27f8ac02181828534a78b
akinsayesokpah82-rgb <akinsayesokpah82@gmail.com>
2025-11-08 02:58:48 +0000

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,4 @@
-const CACHE = "expressai-cache-v1";
-const URLS = ["/", "/index.html", "/manifest.json", "/logo-192.png", "/logo-512.png"];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(URLS)));
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
-});
+const CACHE = "expressai-v1";
+const OFFLINE = ["/","/index.html","/manifest.json","/logo-192.png","/logo-512.png"];
+self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE))));
+self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
